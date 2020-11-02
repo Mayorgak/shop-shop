bfc82a75624db0833d800bf5776396ecc636375e
Kristen M <kristenmayorga@gmail.com>
2020-11-01 22:24:40 -0500

    adde reducer file

---
+++ client/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import commerceReducer from "./commerceReducer.js";
+
+const allReducers = combineReducers({
+  commerce: commerceReducer,
+});
+
+export default allReducers;
